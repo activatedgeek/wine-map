2c736fe8d02dfe619d4340f0da0855d7afc43499
Sanyam Kapoor <3909933+activatedgeek@users.noreply.github.com>
2019-06-12 14:59:10 -0700

    fix modal flash before data loading (#8)

--- src/reducers.js
+++ src/reducers.js
@@ -2,6 +2,13 @@ import { combineReducers } from 'redux';
 import keplerGlReducer from 'kepler.gl/reducers';
 
 
+const keplerGlReducerMod = keplerGlReducer.initialState({
+  uiState: {
+    currentModal: null,
+  },
+});
+
+
 export default combineReducers({
-  keplerGl: keplerGlReducer,
+  keplerGl: keplerGlReducerMod,
 });
